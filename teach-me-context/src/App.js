974a5c2845103206d646f33e5c8e6cbab9bf4a29
pdo9141 <pdo9141@gmail.com>
2018-05-20 20:00:01 -0700

    Adding final code

--- teach-me-context/src/App.js
+++ teach-me-context/src/App.js
@@ -1,19 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// first we will make a new context
+const MyContext = React.createContext();
+
+// then creat3e a provider component
+class MyProvider extends Component {
+  state = {
+    name: 'Wes',
+    age: 100,
+    cool: true
+  }
+
+  render() {
+    return (
+      <MyContext.Provider value={{
+        state: this.state,
+        growAYearOlder: () => this.setState({
+          age: this.state.age + 1
+        })
+      }}>
+        {this.props.children}
+      </MyContext.Provider>
+    )      
+  }
+}
+
+const Family = (props) => (
+  <div className="family">
+    <Person />
+  </div>
+)
+
+class Person extends Component {
+  render() {
+    return (
+      <div className="person">
+        <MyContext.Consumer>
+          {(context) => (
+            <React.Fragment>
+              <p>Age: {context.state.age}</p>
+              <p>Name: {context.state.name}</p>
+              <button onClick={context.growAYearOlder}>Grow Older</button>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MyProvider>
+        <div>
+          <p>I'm the app</p>
+          <Family />
+        </div>
+      </MyProvider>
     );
   }
 }
